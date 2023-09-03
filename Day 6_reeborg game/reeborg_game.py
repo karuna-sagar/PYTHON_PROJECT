52951b699ce6666b82c8bbddb081836a35fe06a7
Karuna sagar <128089073+karuna-sagar@users.noreply.github.com>
2023-09-03 21:53:33 +0530

    Add files via upload

---
+++ Day 6_reeborg game/reeborg_game.py
@@ -0,0 +1,65 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def move_for():
+#     turn_left()
+#     move()
+# def move_upp_for():
+#     move()
+# move()
+# for i in range(1,6):
+#     move_for()
+#     turn_right()
+#     move_upp_for()
+#     turn_right()
+#     move()
+#     move_for()
+# move_for()
+# turn_right()
+# move_upp_for()
+# turn_right()
+# move()
+
+
+# *********************or we can write like  this *************************************
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# for i in range(1,7):
+#     jump()
+
+
+
+
+
+# *******************hurdle 3 **************
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
