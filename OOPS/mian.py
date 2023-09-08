4e763698ddc3a1a6ecd70187fcd70b0db3c5694d
Karuna sagar <128089073+karuna-sagar@users.noreply.github.com>
2023-09-09 02:58:26 +0530

    Add files via upload

---
+++ OOPS/mian.py
@@ -0,0 +1,13 @@
+from turtle import Turtle,Screen
+# answer = Turtle()
+# print(answer)
+# answer.shape("turtle")
+# answer.color("red")
+# answer.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+print(table)
